58547b05d2637e68378a1066f9c7b3f485101b06
Andre <patriqdesigns@gmail.com>
2023-02-05 02:29:38 +0100

    Fly deploy. Changed to memory session storage.

--- web/shopify.js
+++ web/shopify.js
@@ -2,8 +2,7 @@ import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-01";
-
-const DB_PATH = `${process.cwd()}/database.sqlite`;
+import { MemorySessionStorage } from "@shopify/shopify-app-session-storage-memory";
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
@@ -29,8 +28,7 @@ const shopify = shopifyApp({
   webhooks: {
     path: "/api/webhooks",
   },
-  // This should be replaced with your preferred storage strategy
-  sessionStorage: new SQLiteSessionStorage(DB_PATH),
+  sessionStorage: new MemorySessionStorage(),
 });
 
 export default shopify;
